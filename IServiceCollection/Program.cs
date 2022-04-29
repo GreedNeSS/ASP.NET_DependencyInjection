a36a2b3752acd54ed85e6ac75d6b31a89b4fb09b
GreedNeSS <ruslan199126@gmail.com>
2022-04-29 19:46:27 +0300

    Added IServiceCollection example.

--- IServiceCollection/Program.cs
+++ IServiceCollection/Program.cs
@@ -1,6 +1,26 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+IServiceCollection services = builder.Services;
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async context =>
+{
+    StringBuilder sb = new StringBuilder();
+    sb.Append("<h1>Все сервисы!</h1>");
+    sb.Append("<table><tr><th>Тип</th><th>LifeTime</th><th>Реализация</th></tr>");
+
+    foreach (var service in services)
+    {
+        sb.Append($"<tr><th>{service.ServiceType.Name}</th>");
+        sb.Append($"<th>{service.Lifetime}</th>");
+        sb.Append($"<th>{service.ImplementationType?.FullName}</th></tr>");
+    }
+
+    sb.Append("</tr></table>");
+
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync(sb.ToString());
+});
 
 app.Run();
